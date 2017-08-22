62502a450bc3908d68366a11376891cf4327c318
jiarenlu <gunbanmi@gmail.com>
2017-08-22 23:59:05 +0800

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
 
-  resources :articles
+
+  resources :articles do
+    resources :comments, only: [:index, :new, :create]
+  end
+  resources :comments, only: [:show, :edit, :update, :destroy]
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
